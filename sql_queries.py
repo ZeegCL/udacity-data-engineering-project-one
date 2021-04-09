fe81ada15617eecb4fcdc331cdd1fd062bc12a2b
José González <hello.zeegcl@gmail.com>
2021-04-09 16:05:35 -0400

    fix: Submission Review n°1: Table creation
    PRIMARY KEYs: Corrects fields are made PRIMARY KEY in all the tables except time table, please go through my comment in the code review on why this is required.
    
    NOT NULLs: Currently, NOT NULL constraints have been not been implemented for correct fields. There are two important columns user_id, start_time in songplays table that should not be NULL. These constraints are close to data and prevent NULL value for columns that must have values.

--- sql_queries.py
+++ sql_queries.py
@@ -11,8 +11,8 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays (
         songplay_id SERIAL PRIMARY KEY,
-        start_time timestamp,
-        user_id int,
+        start_time timestamp NOT NULL,
+        user_id int NOT NULL,
         level varchar,
         song_id varchar,
         artist_id varchar,
@@ -54,7 +54,7 @@ artist_table_create = ("""
 
 time_table_create = ("""
     CREATE TABLE IF NOT EXISTS time (
-        start_time timestamp,
+        start_time timestamp PRIMARY KEY,
         hour int,
         day int,
         week int,
