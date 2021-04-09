d4e9595dfc33e136da874f684517aff020ecaf81
José González <hello.zeegcl@gmail.com>
2021-04-09 16:06:48 -0400

    fix: Submission Review n°2: ETL
    For the user_table_insert you have put DO NOTHING when there is a conflict. But would we like to ignore it? The level field is used for free and paid services for a user. Consider someone subscribes by paying a fee who happened to be a free user before. In that scenario, we would like to update the level of the user to paid. The current implementation will ignore the update, which is not desired. You can do that by ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level

--- sql_queries.py
+++ sql_queries.py
@@ -74,7 +74,7 @@ songplay_table_insert = ("""
 user_table_insert = ("""
     INSERT INTO users (user_id, first_name, last_name, gender, level)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT DO NOTHING;
+    ON CONFLICT user_id DO UPDATE SET level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
@@ -92,7 +92,8 @@ artist_table_insert = ("""
 
 time_table_insert = ("""
     INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-    VALUES (%s, %s, %s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 # FIND SONGS
